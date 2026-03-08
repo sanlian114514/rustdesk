072ecd6ea5775f7927f0b6512abf7687cbeda822
sanlian114514 <sanlian114514@outlook.com>
2026-03-09 00:31:57 +0800

    maybe fix flutter hide CM

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -26,6 +26,7 @@ import 'package:url_launcher/url_launcher_string.dart';
 
 import '../../common/widgets/dialog.dart';
 import '../../common/widgets/login.dart';
+import '../../models/server_model.dart';
 
 const double _kTabWidth = 200;
 const double _kTabHeight = 42;
@@ -1389,6 +1390,8 @@ class _SafetyState extends State<_Safety> with AutomaticKeepAliveClientMixin {
     return ChangeNotifierProvider.value(
         value: gFFI.serverModel,
         child: Consumer<ServerModel>(builder: (context, model, child) {
+          final enableHideCm = model.approveMode == 'password' &&
+              (model.verificationMethod == kUsePermanentPassword || model.verificationMethod == kUseBothPasswords);
           onHideCmChanged(bool? b) {
             if (b != null) {
               bind.mainSetOption(
@@ -1397,29 +1400,30 @@ class _SafetyState extends State<_Safety> with AutomaticKeepAliveClientMixin {
           }
 
           return Tooltip(
-              message: translate('hide_cm_tip'),
+              message: enableHideCm ? "" : translate('hide_cm_tip'),
               child: GestureDetector(
                 onTap:
-                    () => onHideCmChanged(!model.hideCm),
+                    enableHideCm ? () => onHideCmChanged(!model.hideCm) : null,
                 child: Row(
                   children: [
                     Checkbox(
                             value: model.hideCm,
-                            onChanged: enabled ? onHideCmChanged : null)
+                            onChanged: enabled && enableHideCm
+                                ? onHideCmChanged
+                                : null)
                         .marginOnly(right: 5),
                     Expanded(
                       child: Text(
                         translate('Hide connection management window'),
                         style: TextStyle(
                             color: disabledTextColor(
-                                context, enabled)),
+                                context, enabled && enableHideCm)),
                       ),
                     ),
                   ],
                 ),
               ));
         }));
-  }
 
   List<Widget> autoDisconnect(BuildContext context) {
     TextEditingController controller = TextEditingController();
